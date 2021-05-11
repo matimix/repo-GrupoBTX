b89b25493902857430330876f40e88d7d592c6a9
Familia <Familia@192.168.0.7>
2021-05-11 18:57:26 -0300

    clase modelo creada llamada compra con sus atributos correspondientes su constructor y sus metodos accesorios

---
+++ Cruz_FrancoMatias_2931_TP4/src/main/java/ar/edu/unju/fi/tp4/model/Compra.java
@@ -0,0 +1,78 @@
+package ar.edu.unju.fi.tp4.model;
+
+public class Compra {
+	private int id;
+	private Producto producto;
+	private int cantidad;
+	private double total;
+	
+	public Compra() {
+		
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the producto
+	 */
+	public Producto getProducto() {
+		return producto;
+	}
+
+	/**
+	 * @param producto the producto to set
+	 */
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+
+	/**
+	 * @return the cantidad
+	 */
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	/**
+	 * @param cantidad the cantidad to set
+	 */
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	/**
+	 * @return the total
+	 */
+	public double getTotal() {
+		return total;
+	}
+
+	/**
+	 * @param total the total to set
+	 */
+	public void setTotal(double total) {
+		this.total = total;
+	}
+
+	@Override
+	public String toString() {
+		return "Compra [id=" + id + ", producto=" + producto + ", cantidad=" + cantidad + ", total=" + total + "]";
+	}
+	
+	
+	
+	
+
+}
