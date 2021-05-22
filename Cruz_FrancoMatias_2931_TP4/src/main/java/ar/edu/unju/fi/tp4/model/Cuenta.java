9ee782fab406224422dd9da00f6a74bdf1d3bbe6
Familia <Familia@192.168.0.7>
2021-05-22 03:18:31 -0300

    Se agregan los atributos de la clase cuenta

--- Cruz_FrancoMatias_2931_TP4/src/main/java/ar/edu/unju/fi/tp4/model/Cuenta.java
+++ Cruz_FrancoMatias_2931_TP4/src/main/java/ar/edu/unju/fi/tp4/model/Cuenta.java
@@ -1,13 +1,21 @@
 package ar.edu.unju.fi.tp4.model;
 
+import java.time.LocalDate;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Component;
 
 @Component
 @Entity
-@Table
+@Table(name = "CUENTAS")
 public class Cuenta {
+	private Long id;
+	private Double saldo;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate fechaDeCreacion;
+	private String estado;
 
 }
