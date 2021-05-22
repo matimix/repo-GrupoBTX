a917a5bd405a2f42ea19569ae53b8508c5d872af
Familia <Familia@192.168.0.7>
2021-05-22 03:25:24 -0300

    se agregan los set y get de la clase cuenta

--- Cruz_FrancoMatias_2931_TP4/src/main/java/ar/edu/unju/fi/tp4/model/Cuenta.java
+++ Cruz_FrancoMatias_2931_TP4/src/main/java/ar/edu/unju/fi/tp4/model/Cuenta.java
@@ -17,5 +17,73 @@ public class Cuenta {
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDate fechaDeCreacion;
 	private String estado;
+	
+	public Cuenta() {
+		// Auto-generated
+	}
 
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the saldo
+	 */
+	public Double getSaldo() {
+		return saldo;
+	}
+
+	/**
+	 * @param saldo the saldo to set
+	 */
+	public void setSaldo(Double saldo) {
+		this.saldo = saldo;
+	}
+
+	/**
+	 * @return the fechaDeCreacion
+	 */
+	public LocalDate getFechaDeCreacion() {
+		return fechaDeCreacion;
+	}
+
+	/**
+	 * @param fechaDeCreacion the fechaDeCreacion to set
+	 */
+	public void setFechaDeCreacion(LocalDate fechaDeCreacion) {
+		this.fechaDeCreacion = fechaDeCreacion;
+	}
+
+	/**
+	 * @return the estado
+	 */
+	public String getEstado() {
+		return estado;
+	}
+
+	/**
+	 * @param estado the estado to set
+	 */
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	@Override
+	public String toString() {
+		return "Cuenta [id=" + id + ", saldo=" + saldo + ", fechaDeCreacion=" + fechaDeCreacion + ", estado=" + estado
+				+ "]";
+	}
+
+	
+	
 }
